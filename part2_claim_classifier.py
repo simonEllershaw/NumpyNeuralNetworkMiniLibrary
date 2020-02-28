b6b4c0b3463ae1474e966ea4cf52ae2e666195b8
bjp19 <be1994JP>
2020-02-28 13:57:37 +0000

    BP - Modelled best model trained

--- part2_claim_classifier.py
+++ part2_claim_classifier.py
@@ -214,7 +214,7 @@ def ClaimClassifierHyperParameterSearch(data_x, data_y, test_x, test_y, variable
     The function should return your optimised hyper-parameters.
     """
     max_metric = 0
-    searches = 2
+    searches = 100
 
     for i in range(searches):
         multiplier = round(np.random.uniform(1, 15))
