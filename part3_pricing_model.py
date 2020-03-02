4fd4dd2f1738e1bfd1f661a41c8901e29bd1e4d3
bjp19 <benjamin.prior19@imperial.ac.uk>
2020-03-02 18:25:30 +0000

    BP - Best Pickle file for part3

--- part3_pricing_model.py
+++ part3_pricing_model.py
@@ -231,7 +231,7 @@ class PricingModel():
         # =============================================================
         # REMEMBER TO INCLUDE ANY PRICING STRATEGY HERE.
         # For example you could scale all your prices down by a factor
-        premium_factor = 0.201
+        premium_factor = 0.20
         premiums = self.predict_claim_probability(X_raw) * self.y_mean * premium_factor
         premiums = np.array(premiums)
         premiums = premiums.flatten()
